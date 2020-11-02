5cdc4ea350dfc001c1347f25117393576bc8d456
Eric <ericalpizar@icloud.com>
2020-11-02 15:46:30 -0600

    Added uint8_mult.inc file and the cli implemented in main.asm

--- ASM/algoritmo_mult/main.asm
+++ ASM/algoritmo_mult/main.asm
@@ -2,6 +2,7 @@ include C:\Irvine\Irvine32.inc
 includelib C:\Irvine\Irvine32.lib
 includelib C:\Irvine\Kernel32.lib
 includelib C:\Irvine\user32.lib
+include uint8_mult.inc
 
 ; Tecnologico de Costa Rica
 ;
@@ -20,87 +21,52 @@ ExitProcess PROTO, dwExitCode:DWORD
 
 .data
 
-welcome_message BYTE "Welcome to the multiplication algorithm", 0				;The welcome message is stored
-
-num_1 BYTE ?
-num_2 BYTE ?
-cont BYTE ?
-res BYTE ?
-temp DWORD ?
-
+welcome_message BYTE "----------Welcome to the uint8_mult algorithm----------", 0				;The welcome message is stored
+prompt_1 BYTE "Please enter the multiplicand: ", 0												; Prompts are saved
+prompt_2 BYTE "Please enter the multiplier: ", 0
+multiplicand BYTE ?
+multiplier BYTE ?
 
 .code
 
 main PROC
 
-menu MACRO welcome_message:REQ
+_cli MACRO welcome_message, prompt_1, prompt_2													;the cli macro is instantiated
 
-	mov edx, OFFSET welcome_message
-	call WriteString
+	mov edx, OFFSET welcome_message																;the welcolme message is moved to edx
+	call WriteString																			;the message is printed in console using Irvine library
+	call CrLf																					;A endline is made on console
 	call CrLf
 
-ENDM
+	mov edx, OFFSET prompt_1																	
+	call WriteString
+	CALL ReadHex																				;A number is requested to the user in console
+	mov multiplicand, al																		;the low 8 bit register is taken
 
-modulo MACRO num_1:REQ, num_2:REQ
-							
-	MOV CH, CL
+	xor eax, eax																				;the eax register is cleaned
 
-	XOR DH, DH						; The dh egister is cleaned using xor
-	MOV AH, num_1					; ah register is used as dividend
-	MOV CL, num_2					; the divisor or quotient is instantiated in cl
-	DIV CL							
-	MOV AH, DH						; the remainder is assigned to ah
-	MOV CL, CH						; Counter register is restores with the initial value
-
-ENDM
-
-
-uint8_mult MACRO num_1:REQ, num_2:REQ, res
-
-
-	mov ah, num_1						; the num_1 is moved to the ah 8 bit counter
-	mov bh, num_2						; num_2 is instantiated in bh wich is an 8 bit num_2 register
-	mov cl, 0							; the 8 bit count register is assigned to 0
-	movzx temp, num_2
-
-	movzx ecx, num_2
-
-	_while:
-		
-		movzx  temp, ecx
-
-		modulo num_1,num_2				;modulo operation is applied to num_1 and num_2
-
-		cmp ah, 1						; cheks if the remainder is equal to one, this is done to determine if the numbers where even or not
-
-		je _shift_left
-
-			_shift_left:
-
-				shl num_1, cl			; a left shift is done with the value that the counter register has
-
-		inc cl							; Increments the 8 bit counter register
-		XOR edx, edx					; The dh egister is cleaned using xor
-
-		mov ah, num_1
-		
-		mov ebx, 2				
-		div ebx							;divides de num_2 by two
-
-
-		cmp eax, 0
-
-		ja _while
-
-		je endwhile
-
-	endwhile:
-
-		CALL DumpRegs
+	mov edx, OFFSET prompt_2																
+	call WriteString
+	CALL ReadHex																				;the multiplier is read from console
+	mov multiplier, al
+	CALL CrLf
 
 ENDM
 
- INVOKE ExitProcess,0
+_cli welcome_message, prompt_1, prompt_2														;the cli is excecuted and parameters are asked to the user
+
+xor eax, eax																					;the registers are cleaned
+xor ebx, ebx
+
+mov al, multiplicand																			;multiplicand is moved to the 8 low bit AL register (Acumulator)
+mov bl, multiplier																				;mltiplier is moved to the 8 low bit BL register    (Base register)
+
+
+INVOKE uint8_mul																				;the uint8_mul is invoked
+
+CALL DumpRegs																					;registers are showned on console
+
+INVOKE ExitProcess,0
 
 main ENDP
 
