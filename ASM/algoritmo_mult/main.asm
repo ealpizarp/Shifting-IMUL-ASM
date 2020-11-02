9000911e4e36aa7e6a0614e7d196d36b4969fe2b
Eric Alpizar Prendas <ericalpizar@icloud.com>
2020-11-01 21:23:25 -0600

    Fixed some problems and added more documentation

--- ASM/algoritmo_mult/main.asm
+++ ASM/algoritmo_mult/main.asm
@@ -26,6 +26,7 @@ num_1 BYTE ?
 num_2 BYTE ?
 cont BYTE ?
 res BYTE ?
+temp DWORD ?
 
 
 .code
@@ -49,40 +50,49 @@ modulo MACRO num_1:REQ, num_2:REQ
 	MOV CL, num_2					; the divisor or quotient is instantiated in cl
 	DIV CL							
 	MOV AH, DH						; the remainder is assigned to ah
-	MOV CL, CH
+	MOV CL, CH						; Counter register is restores with the initial value
 
 ENDM
 
 
 uint8_mult MACRO num_1:REQ, num_2:REQ, res
 
+
 	mov ah, num_1						; the num_1 is moved to the ah 8 bit counter
 	mov bh, num_2						; num_2 is instantiated in bh wich is an 8 bit num_2 register
 	mov cl, 0							; the 8 bit count register is assigned to 0
+	movzx temp, num_2
+
+	movzx ecx, num_2
 
 	_while:
-		mov num_2, bh
+		
+		movzx  temp, ecx
 
 		modulo num_1,num_2				;modulo operation is applied to num_1 and num_2
 
 		cmp ah, 1						; cheks if the remainder is equal to one, this is done to determine if the numbers where even or not
 
-		jmp _shift_left	
+		je _shift_left
 
-		_shift_left:
-			shl num_1, cl
+			_shift_left:
+
+				shl num_1, cl			; a left shift is done with the value that the counter register has
 
 		inc cl							; Increments the 8 bit counter register
-		XOR DH, DH						; The dh egister is cleaned using xor
-		mov ah, num_1
-		mov bl, 2					
-		div bl							;divides de num_1 by two
+		XOR edx, edx					; The dh egister is cleaned using xor
 
-		cmp bh, 0							
+		mov ah, num_1
+		
+		mov ebx, 2				
+		div ebx							;divides de num_2 by two
+
+
+		cmp eax, 0
 
 		ja _while
 
-		jmp endwhile
+		je endwhile
 
 	endwhile:
 
