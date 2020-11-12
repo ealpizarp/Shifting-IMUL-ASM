5075ab8601aa659b5d11083f538c4db21f771fdb
Eric <ericalpizar@icloud.com>
2020-11-12 11:45:21 -0600

    Made big changes on command line interface and validation improvements

--- ASM/algoritmo_mult/main.asm
+++ ASM/algoritmo_mult/main.asm
@@ -1,16 +1,17 @@
-include C:\Irvine\Irvine32.inc
-includelib C:\Irvine\Irvine32.lib
-includelib C:\Irvine\Kernel32.lib
-includelib C:\Irvine\user32.lib
-include uint8_mult.inc
-
-; Tecnologico de Costa Rica
+; ==========Costa Rica Institute of Technology===========
 ;
 ; Authors: Eric Alpizar
 ;		   Rodrigo Espinach
 ;		   Jimmy Salas
 ;
-; Last date modification: 10/31/2020
+; Last date modification: 11/13/2020
+
+include C:\Irvine\Irvine32.inc
+includelib C:\Irvine\Irvine32.lib
+includelib C:\Irvine\Kernel32.lib
+includelib C:\Irvine\user32.lib
+include uint8_mult.inc
+include cli.inc
 
 .386
 .model flat,stdcall
@@ -20,88 +21,55 @@ include uint8_mult.inc
 ExitProcess PROTO, dwExitCode:DWORD
 
 .data
-
-welcome_message BYTE "----------Welcome to the uint8_mult algorithm----------", 0				;The welcome message is stored
-prompt_1 BYTE "Please enter the multiplicand: ", 0												; Prompts are saved
-prompt_2 BYTE "Please enter the multiplier: ", 0
-prompt_3 BYTE "The result is: ",0
-error_message_multiplicand BYTE "Error with the multiplicand please enter the number again",0		;error message for multiplicand
-error_message_multiplier BYTE "Error with the multiplier please enter the number again",0			;error message for multiplier
-multiplicand BYTE ?
-multiplier BYTE ?
-
+	prompt_0 BYTE "Would you like to calculate again? (Y/N) ", 0	
+	validation_prompt BYTE "Invalid option!", 0
+	final_message BYTE "Thank you for choosing ´uint8_mult´ calculator", 0	
 .code
 
 main PROC
 
+calculation:
+CALL CLRSCR
+XOR EAX, EAX
 
-error_multiplicand:
-	mov edx, OFFSET error_message_multiplicand																;the error_message_multiplicand is moved to edx
-	call WriteString
-	call CrLf																					;A endline is made on console
-	call CrLf
-
-	jmp insert_multiplicando
-
-error_multiplier:
-	mov edx, OFFSET error_message_multiplier																;the error_message_multiplier is moved to edx
-	call WriteString
-	call CrLf																					;A endline is made on console
-	call CrLf
-
-	jmp insert_multiplier
-
-_display_result MACRO prompt_3:REQ
-
-	mov edx, OFFSET prompt_3																	;prompt_3 is moved to the edx register
-	call WriteString																			; edx is printed to screen
-	call WriteDec																				;display the eax register in decimal value
-	CALL CrLf
-
-ENDM
-
-welcome:
-	mov edx, OFFSET welcome_message																;the welcolme message is moved to edx
-	call WriteString																			;the message is printed in console using Irvine library
-	call CrLf																					;A endline is made on console
-	call CrLf
-
-insert_multiplicando:
-	xor eax, eax
-
-	mov edx, OFFSET prompt_1																	
-	call WriteString
-	CALL ReadInt																				;A number is requested to the user in console
-	cmp multiplicand, 255
-	ja error_multiplicand
-	mov multiplicand, eax																		;the low 8 bit register is taken
-
-	cmp 2, 1
-
-insert_multiplier:
-	xor eax, eax																				;the eax register is cleaned
-
-	mov edx, OFFSET prompt_2																
-	call WriteString
-	CALL ReadInt																				;the multiplier is read from console
-	cmp multiplier, 255																			;check the multiplier is a number of 8 bits at most
-	ja error_multiplier
-	mov multiplier, eax
-	CALL CrLf													;the cli is excecuted and parameters are asked to the user
-
-ready_for_mult:
-	xor eax, eax																					;the registers are cleaned
-	xor ebx, ebx
-
-	mov al, multiplicand																			;multiplicand is moved to the 8 low bit AL register (Acumulator)
-	mov bl, multiplier																				;mltiplier is moved to the 8 low bit BL register    (Base register)
-
-
-
+INVOKE _cli
 
 INVOKE uint8_mul																				;the uint8_mul is invoked
 
 _display_result prompt_3																		;the result is displayed on console
+
+
+redo_calculation:
+
+mov edx, OFFSET prompt_0																		;prompt_3 is moved to the edx register
+call WriteString																				;edx is printed to screen
+call ReadChar																					;single character is read form console
+call WriteChar
+
+CALL CRLF																						;an end line is made on console
+cmp al, 89D																						;compares character with upper case Y ascii code
+je calculation																					;jumps to calculation label
+
+cmp al, 121D																					;121D stands for the ascii code of lowercase y
+je calculation
+
+
+cmp al, 110D																					;110D stands for the ascii code of lowercase n
+je _end
+
+cmp al, 78D																						;78D stads fot the ascii code of uppercase N
+je _end
+
+mov edx, OFFSET validation_prompt																;validation_prompt is moved to the edx register
+call WriteString																				;edx register is displayed on console
+call CRLF
+
+jmp redo_calculation																			;jumps to the redo_calculation label
+
+_end:																							;label that indicates the last execution
+
+mov edx, OFFSET final_message																	;final_message is moved to the edx register
+call WriteString
 
 INVOKE ExitProcess,0
 
